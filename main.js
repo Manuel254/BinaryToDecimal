0c469aca8c0a200ff38abf244cdcdbca729c0567
Immanuel Kituku <Immmanuelkituku@gmail.com>
2020-08-28 19:54:44 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,20 @@
+let binary = document.querySelector('.binary');
+let decimal = document.querySelector('.decimal');
+let error =  document.querySelector('.error');
+
+document.querySelector('.btn').addEventListener('click',binToDec);
+
+function binToDec(e){
+    e.preventDefault();
+    // Reverse string
+    const reversed = parseInt(binary.value.split('').reverse());
+    let total = 0;
+
+    for (let i = 0; i < reversed.length; i++){
+        // Raise count to the power of 2
+        const power = Math.pow(2,i);
+        // Multiply result by digit
+        total = total + (reversed[i] * power);
+    }
+    decimal.value = total;
+}
