bcb086a5758ca3d37486f8610c66757ac8701f01
Immanuel Kituku <50909294+Manuel254@users.noreply.github.com>
2020-10-04 08:55:34 +0300

    Update main.js

--- main.js
+++ main.js
@@ -5,15 +5,16 @@ document.querySelector('.btn').addEventListener('click',binToDec);
 
 function binToDec(e){
     e.preventDefault();
-    // Reverse string
-    const reversed = binary.value.split('').reverse();
-    let total = 0;
+//     // Reverse string
+//     const reversed = binary.value.split('').reverse();
+//     let total = 0;
 
-    for (let i = 0; i < reversed.length; i++){
-        // Raise count to the power of 2
-        const power = Math.pow(2,i);
-        // Multiply result by digit
-        total = total + (reversed[i] * power);
-    }
-    decimal.value = total;
+//     for (let i = 0; i < reversed.length; i++){
+//         // Raise count to the power of 2
+//         const power = Math.pow(2,i);
+//         // Multiply result by digit
+//         total = total + (reversed[i] * power);
+//     }
+//     decimal.value = total;
+    decimal.value = ParseInt(binary.value,2);
 }
